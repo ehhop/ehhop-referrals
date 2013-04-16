ff182df3394f1015e8a0b99cf260b73f8eb03b9c
Ted Pak <powerpak006@gmail.com>
2013-04-16 15:13:14 -0400

    fixed the python27 upgrade

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 # http://www.labnol.org/
 
 import os
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+import webapp2
 from google.appengine.ext.webapp import template
 
-class MainHandler(webapp.RequestHandler):
+class MainHandler(webapp2.RequestHandler):
   def get (self, q):
     if q is None:
       q = 'index.html'
@@ -15,9 +14,4 @@ class MainHandler(webapp.RequestHandler):
     self.response.headers ['Content-Type'] = 'text/html'
     self.response.out.write (template.render (path, {}))
 
-def main ():
-  application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
-  util.run_wsgi_app (application)
-
-if __name__ == '__main__':
-  main ()
+app = webapp2.WSGIApplication([('/(.*html)?', MainHandler)])
