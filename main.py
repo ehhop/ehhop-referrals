5613e125a79a87b096452ccc3d96489316c3de1f
Ted Pak <powerpak006@gmail.com>
2014-01-05 22:59:14 -0500

    redirect to the new EHHapp

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ class MainHandler(webapp2.RequestHandler):
   def get (self, q):
     if q is None:
       q = 'index.html'
+     
+    if q == 'index.html':
+      self.redirect('http://ehhapp.org', permanent=True)
 
     path = os.path.join (os.path.dirname (__file__), q)
     self.response.headers ['Content-Type'] = 'text/html'
